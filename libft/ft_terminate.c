77bd854bd14f0b1c005ca28cb46c7d9ed21ee9d4
flowjiyun <gy2795@gmail.com>
2023-02-08 17:08:03 +0900

    fix: change error message

--- libft/ft_terminate.c
+++ libft/ft_terminate.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 12:11:28 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/12/07 17:29:13 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2023/02/08 16:59:45 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ void	ft_terminate(const char *str)
 {
 	const char	*err = strerror(errno);
 
+	ft_putendl_fd("Error", 2);
 	if (errno == 0)
 		ft_putstr_fd((char *)str, 2);
 	else
-		ft_putstr_fd((char *)err, 1);
+		ft_putstr_fd((char *)err, 2);
 	ft_putstr_fd("\n", 2);
 	exit(1);
 }
